e515dd77f1a60edb7f2cf2df12e3d5ea9d7f6eeb
Student-3 GameSchool <stud3.gsch124@outlook.com>
2025-03-24 16:17:29 +0700

    ПОБЕДА!

--- ConsoleApplication15/ConsoleApplication15.cpp
+++ ConsoleApplication15/ConsoleApplication15.cpp
@@ -13,7 +13,7 @@ vector<string> items_names;
 
 struct portal_
 {
-    int target;
+    int target = 0;
     bool isActive = false;
 };
 
@@ -26,12 +26,11 @@ struct location_ {
     int gold;
 };
 
-location_ location[6];
 
 
 struct player_ {
 
-    int location = 0;
+    int loc = 0;
     vector<items_> items;
     int gold;
     bool life = true;
@@ -39,16 +38,59 @@ struct player_ {
 
 };
  
+    
+location_ location[3];
+player_ player;
+
+void InitGame()
+{
+    location[0].name = "stone"; 
+    
+    location[0].portal.push_back({ 2, true });
+    location[0].portal.push_back({ 1, false });
+    location[1].name = "front";
+    location[1].portal.push_back({ 0, true });
+    location[1].portal.push_back({ 2, true });
+    location[2].name = "back";
+    location[2].portal.push_back({ 0, true });
+    location[2].portal.push_back({ 1, false });
+
+    portal_ a { 0, true };
+    portal_ b { 1, true };
+    portal_ z { 2, false };
+}
 
 int main() {
+    InitGame();
+    setlocale(LC_ALL, "Russian");
+    while (player.life)
+    {
+        auto i = player.loc;
+        cout << location[player.loc].name << "\n";
+
+        string command;
+        cin >> command;
+
+        if (command == "go")
+        {
+
+            for (int j = 0; j < location->portal.size(); j++)
+            {
+             cout << "Портал: " << j+1 << " ведет в локацию " << location[j+1].name << endl;
+            }
+
+            int selected_way;
+            cin >> selected_way;
+
+            if (selected_way == 1 )
+            {
+                player.loc = location[player.loc].portal[selected_way].target;
+                cout << "Вы перешли в локацию " << location[player.loc].name << endl;
+            }
 
 
-    portal_ a = { 9 , true };
-    portal_ b = { 2, false };
-    
-    int i = 9, z = 0;
-
-    string;
+        }
+    }
 
     
     
